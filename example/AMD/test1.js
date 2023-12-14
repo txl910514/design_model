e738fe7a7f17db772f08b51990ff2e6f91e3c8a1
tangxl <1092678514@qq.com>
2023-12-14 15:23:42 +0800

    提交代码

---
+++ example/AMD/test1.js
@@ -0,0 +1,9 @@
+define(
+    "test1",
+    [], function() {
+    
+    var test1Obj = {
+        a: 4
+    }
+    return test1Obj
+});
---
+++ example/AMD/test2.js
@@ -0,0 +1,9 @@
+define(
+    "test2",
+    [], function() {
+    
+    var test1Obj = {
+        b: 5
+    }
+    return test1Obj
+});
